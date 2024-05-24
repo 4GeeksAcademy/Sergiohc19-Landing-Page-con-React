f8609d91e8ce40c0a231515cd7822214d547f779
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-05-24 15:17:47 +0000

    Proyecto terminado

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -8,7 +8,7 @@ const Jumbotron = () => {
         <h1 className="display-4 d-flex justify-content-start col-6">A Warm Welcome !</h1>
         <p className="d-flex justify-content-start col-12">It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <div className="pb-1">
-          <button type="button" class="btn btn-primary d-flex justify-content-start">Primary</button>
+          <button type="button" class="btn btn-primary d-flex justify-content-start">Call to action!</button>
         </div>
       </div>
     </div>
